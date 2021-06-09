82cdaa86c06102c0326d57139dc13e9af2bc92af
rivasty <49377307+rivasty@users.noreply.github.com>
2021-06-08 21:36:25 -0700

    added getuserbyemail

--- db/dbCall.js
+++ db/dbCall.js
@@ -3,15 +3,21 @@ const mango = require('../lib/mango');
 const val   = require('../lib/validation');
 // DB CALLS
 async function getUserCount() {
-	return 0;
+	const db = mango.getDBReference();
+	const collection = db.collection('users');
+	return await collection.count();
 }
 async function getVideoCount() {
-	return 0;
+	const db = mango.getDBReference();
+	const collection = db.collection('videos');
+	return await collection.count();
 }
 async function getCommentCount() {
-	return 0
+	const db = mango.getDBReference();
+	const collection = db.collection('comments');
+	return await collection.count();
 }
-async function insertUser(user) {
+async function insertU(user) {
 	const db = mango.getDBReference();
 	const collection = db.collection('users');
 	user = val.extractValidFields(user,val.userschema);
@@ -20,7 +26,7 @@ async function insertUser(user) {
 	console.log('insert result:',result);
 	return result;
 }
-async function insertComment(comment) {
+async function insertC(comment) {
 	const db = mango.getDBReference();
 	const collection = db.collection('comments');
 	comment = val.extractValidFields(comment,val.commentschema);
@@ -29,7 +35,7 @@ async function insertComment(comment) {
 	console.log('insert result:',result);
 	return result;
 }
-async function insertVideo(video) {
+async function insertV(video) {
 	const db = mango.getDBReference();
 	const collection = db.collection('videos');
 	video = val.extractValidFields(video,val.videoschema);
@@ -38,7 +44,7 @@ async function insertVideo(video) {
 	console.log('insert result:',result);
 	return result;
 }
-async function getUserById(id) {
+async function getUById(id) {
 	const db = mango.getDBReference();
 	const collection = db.collection('users');
 	if(ObjectId.isValid(id)) {
@@ -52,9 +58,18 @@ async function getUserById(id) {
 		return null;
 	}
 }
-async function getVideoById(id) {
+async function getUByE(useremail) {
 	const db = mango.getDBReference();
 	const collection = db.collection('users');
+	const result = await collection.find({
+		email: useremail
+	}).toArray();
+	console.log('insert result:',result[0]);
+	return result[0];
+}
+async function getVById(id) {
+	const db = mango.getDBReference();
+	const collection = db.collection('video');
 	if(ObjectId.isValid(id)) {
 		const result = collection.find({
 			_id: id
@@ -66,11 +81,11 @@ async function getVideoById(id) {
 		return null;
 	}
 }
-async function getCommentById(id) {
+async function getCById(id) {
 	const db = mango.getDBReference();
 	const collection = db.collection('users');
 	if(ObjectId.isValid(id)) {
-		const result = collection.find({
+		const result = await collection.find({
 			_id: id
 		}).toArray();
 		console.log('insert result:',result[0]);
@@ -80,37 +95,38 @@ async function getCommentById(id) {
 		return null;
 	}
 }
-async function getUserVideos(id) {
+async function getUV(userid) {
 	const db = mango.getDBReference();
-	const collection = db.collection('users');
+	const collection = db.collection('videos');
 	return null;
 }
-async function getUserComments(userid) {
+async function getUC(userid) {
 	const db = mango.getDBReference();
 	const collection = db.collection('comments');
 	return null;
 }
-async function getCommentsByVideo(id) {
+async function getCByV(videoid) {
 	const db = mango.getDBReference();
 	const collection = db.collection('comments');
 	return null;
 }
 // inserts
-exports.insertUser         = null; // 
-exports.insertComment      = null; // 
-exports.insertVideo        = null; // 
+exports.insertUser         = insertU;  // insert a user
+exports.insertComment      = insertC;  // insert a comment
+exports.insertVideo        = insertV;  // insert a video
 // gets
-exports.getUserById        = null; // 
-exports.getVideoById       = null; // 
-exports.getCommentById     = null; // 
-exports.getUserVideos      = null; // 
-exports.getUserComments    = null; // 
-exports.getCommentsByVideo = null; // 
+exports.getUserById        = getUById; // get user by userid
+exports.getUserByEmail     = getUByE;  // get user by email
+exports.getVideoById       = getVById; // get video by videoid
+exports.getCommentById     = getCById; // get comment by commentid
+exports.getUserVideos      = getUV;    // get videos with userid
+exports.getUserComments    = getUC;    // get comments with userid
+exports.getCommentsByVideo = getCByV;  // get comments with videoid
 // updates
-exports.updateVideo        = null; // 
-exports.updateComment      = null; // 
-exports.updateUser         = null; // 
+exports.updateVideo        = null; // update a video object in db
+exports.updateComment      = null; // update a comment object in db
+exports.updateUser         = null; // update a user object in db
 // deletes
-exports.deleteUser         = null; // 
-exports.deletevideo        = null; // 
-exports.deleteComment      = null; // 
+exports.deleteUser         = null; // delete a user object from db
+exports.deleteVideo        = null; // delete a video object from db
+exports.deleteComment      = null; // delete a comment object from db
--- db/dbinit.js
+++ db/dbinit.js
@@ -1,6 +1,19 @@
 const mango = require('../lib/mango');
-mango.connectToDB(() => {
+const dbjson = require('./data/db.json');
+const dbcall = require('./dbcall');
+mango.connectToDB(async () => {
 	// Fill the db with data from a json
 	console.log("== connected to db");
-	
+	const dbujson = dbjson.users;
+	const dbvjson = dbjson.videos;
+	const dbcjson = dbjson.comments
+	for(c = 0;c < dbujson.length;c++) {
+		await dbcall.insertUser(dbujson[c]);
+	}
+	for(c = 0;c < dbvjson.length;c++) {
+		await dbcall.insertVideo(dbvjson[c]);
+	}
+	for(c = 0;c < dbcjson.length;c++) {
+		await dbcall.insertComment(dbcjson[c]);
+	}
 });
