5946e86350940f1285178af6db58d038164301e8
rivasty <49377307+rivasty@users.noreply.github.com>
2021-06-09 01:22:32 -0700

    more db functions and initdb.js

--- api/index.js
+++ api/index.js
@@ -1,15 +1,9 @@
 const router = module.exports = require('express').Router();
 
 // DELETE ME BEFORE FINAL SUBMISSION
-const dbcall = require('../db/dbcall');
+const dbcall = require('../db/dbCall');
 router.get("/dbtest",async function(req,res,next) {
-	user = {
-		email:"a@b.com",
-		name:"noelle",
-		pass:"fischerPriceTurtle",
-		birth:"10/31/1995"
-	}
-	const result = await dbcall.getUserByEmail("a@b.com");
+	const result = await dbcall.getUserByEmail("noelle@mail.com");
 	console.log("result",result);
 	res.status(201).send({
 		result:"the query was completed!"
--- db/dbinit.js
+++ db/dbinit.js
@@ -1,19 +1,33 @@
-const mango = require('../lib/mango');
+const mango  = require('../lib/mango');
 const dbjson = require('./data/db.json');
-const dbcall = require('./dbcall');
+const dbcall = require('./dbCall');
+const print  = dbcall.print;
 mango.connectToDB(async () => {
 	// Fill the db with data from a json
-	console.log("== connected to db");
+	print("== connected to db");
 	const dbujson = dbjson.users;
 	const dbvjson = dbjson.videos;
 	const dbcjson = dbjson.comments
 	for(c = 0;c < dbujson.length;c++) {
+		// print(dbujson[c]);
 		await dbcall.insertUser(dbujson[c]);
 	}
-	for(c = 0;c < dbvjson.length;c++) {
-		await dbcall.insertVideo(dbvjson[c]);
-	}
-	for(c = 0;c < dbcjson.length;c++) {
-		await dbcall.insertComment(dbcjson[c]);
+	const user = await dbcall.getUserByEmail("noelle@mail.com");
+	if(user) {
+		for(c = 0;c < dbvjson.length;c++) {
+			dbvjson[c].userid = user._id;
+			// print(dbvjson[c]);
+			await dbcall.insertVideo(dbvjson[c]);
+		}
+		const videos = await dbcall.getUserVideos(user._id);
+		if(videos) {
+			const video = videos[0];
+			for(c = 0;c < dbcjson.length;c++) {
+				dbcjson[c].userid = user._id;
+				dbcjson[c].videoid = video._id;
+				// print(dbcjson[c]);
+				await dbcall.insertComment(dbcjson[c]);
+			}
+		}
 	}
 });
