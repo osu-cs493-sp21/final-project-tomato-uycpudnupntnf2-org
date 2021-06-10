d6f3d5dd4d2d3e774841330da602b9d947a05967
yanyan2019 <leeya@oregonstate.edu>
2021-06-09 23:36:50 -0700

    add initial timestamp to videos and comments in DB

--- api/videos.js
+++ api/videos.js
@@ -8,6 +8,7 @@ const {
 
 
 const { getVideoPage } = require('../models/videos')
+const { getVideoById } = require('../db/dbCall')
 
 exports.router = router
 
@@ -25,3 +26,52 @@ router.get('/', async (req, res, next) => {
         });
     }
 });
+
+// get video by video id
+router.get('/:id', async (req, res, next) =>{
+    // console.log(" == timestamp: " , (new Date(Date.now()).toString()))
+    // requested id
+    const vid = req.params.id;
+    console.log(" -- vid: " , vid)
+
+    try {
+        const video = await getVideoById(vid);
+        console.log(' -- video from db: ', video)
+        if(video){
+            res.status(200).send(video);
+        }else{
+            next();
+        }        
+    } catch (error) {
+        console.log(" === ERROR: ", error);
+        res.status(500).send({
+            err: "Unable to fetch video " + vid 
+        })
+    }
+})
+
+
+
+// post video
+router.post('/', async (req, res, next) =>{
+    if(validateSchema(req.body, videoschema)){
+        try {
+            
+        } catch (error) {
+            console.log("  === ERROR: ",error);
+           res.status(500).send({
+                err: "Unable to post video, try again."
+           }) 
+        }
+    }else{
+        res.status(400).send({
+            error: "Request video contents invalid."
+        })
+    }
+})
+
+// put video by vidoe id
+
+// delete video by video id
+
+// likes videos by  video and user id
